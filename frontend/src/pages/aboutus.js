1cfc52cae755921c62294db2dfe3b60d74861a51
Simo00m <166640905+Simo00m@users.noreply.github.com>
2024-11-27 09:03:37 -0700

    adding additional files

---
+++ frontend/src/pages/aboutus.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>I am a react developer.</p>
+    </>
+  );
+};
+
+export default AboutUs;
---
+++ frontend/src/pages/login.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <h1>Login</h1>
+      <p> <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Group 3</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <video src="video.mp4" autoplay loop muted></video>
+
+    <div class="container">
+        <div class="login-form">
+            <form action="" method="get">
+                <h2>login</h2>
+                <div class="form-group">
+                    <input type="email" required>
+                    <label for="">email</label>
+                    <i class="fa-solid fa-envelope"></i>
+                </div>
+                <div class="form-group">
+                    <input type="password" required>
+                    <label for="">password</label>
+                    <i class="fa-solid fa-lock"></i>
+                </div>
+                <p><input type="checkbox"> remember me</p>
+                <input type="button" id="btn" value="login">
+                <p>don't have a account <a href="#">sign up</a></p>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html></p>
+    </>
+  );
+};
+
+export default Login;
