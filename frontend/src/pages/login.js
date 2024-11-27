ef98f4b33bee1caa1c4683106b54334ec255960b
Simo00m <166640905+Simo00m@users.noreply.github.com>
2024-11-27 09:26:10 -0700

    make some changes

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,48 +1,36 @@
 import React from "react";
+import "./style.css"; 
 
 const Login = () => {
   return (
     <>
-      <h1>Login</h1>
-      <p> <!DOCTYPE html>
-<html lang="en">
+      {/* Background Video */}
+      <video src="/video.mp4" autoPlay loop muted></video> 
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project Group 3</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
-        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-
-<body>
-    <video src="video.mp4" autoplay loop muted></video>
-
-    <div class="container">
-        <div class="login-form">
-            <form action="" method="get">
-                <h2>login</h2>
-                <div class="form-group">
-                    <input type="email" required>
-                    <label for="">email</label>
-                    <i class="fa-solid fa-envelope"></i>
-                </div>
-                <div class="form-group">
-                    <input type="password" required>
-                    <label for="">password</label>
-                    <i class="fa-solid fa-lock"></i>
-                </div>
-                <p><input type="checkbox"> remember me</p>
-                <input type="button" id="btn" value="login">
-                <p>don't have a account <a href="#">sign up</a></p>
-            </form>
+      <div className="container">
+        <div className="login-form">
+          <h2>Login</h2>
+          <form action="" method="get">
+            <div className="form-group">
+              <input type="email" required />
+              <label>Email</label>
+              <i className="fa-solid fa-envelope"></i> {/* Font Awesome Icon */}
+            </div>
+            <div className="form-group">
+              <input type="password" required />
+              <label>Password</label>
+              <i className="fa-solid fa-lock"></i> {/* Font Awesome Icon */}
+            </div>
+            <p>
+              <input type="checkbox" /> Remember me
+            </p>
+            <input type="button" id="btn" value="Login" />
+            <p>
+              Don't have an account? <a href="#">Sign up</a>
+            </p>
+          </form>
         </div>
-    </div>
-</body>
-
-</html></p>
+      </div>
     </>
   );
 };
